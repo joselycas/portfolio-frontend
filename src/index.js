fda0acc5bb9a4b56b1b12e4eb531f50fd6a2bb81
Josely castro <joselycas@gmail.com>
2020-02-13 14:53:12 -0500

    create html

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
 import Form from "./components/Form";
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>How to set up React, Webpack, and Babel</title>
+</head>
+<body>
+<div id="container"></div>
+</body>
+</html>
