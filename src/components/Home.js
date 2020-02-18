5457c64b28f86237a953afa997e0bb6658a1996a
Josely castro <joselycas@gmail.com>
2020-02-18 10:28:14 -0500

    add material ui navbar

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import NavBar from "./NavBar"
 
 
 
@@ -7,7 +8,8 @@ const Home = () => {
   return(
     <div>
       <h1>FULL-STACK WEB DEVELOPER AND DIGITAL STRATEGIST.</h1>
-
+      <NavBar />
+      <h2>Little bit about me and who I am.</h2>
     </div>
 
   )
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,22 @@
 import React from "react";
+import Tabs from '@material-ui/core/Tabs';
+import AppBar from '@material-ui/core/AppBar';
+import Tab from '@material-ui/core/Tab';
+
+
+
 
 const NavBar = () => {
  return(
-     <div><h1>NavBar</h1></div>
+     <>
+    <AppBar position="static">
+    <Tabs >
+      <Tab label="Item One" />
+      <Tab label="Item Two" />
+      <Tab label="Item Three" />
+    </Tabs>
+  </AppBar>
+  </>
  )
 }
 
