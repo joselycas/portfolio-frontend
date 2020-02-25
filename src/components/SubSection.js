d57a07b1b73cce12be453bb8b1d5adc9c3bd609f
Josely castro <joselycas@gmail.com>
2020-02-25 18:22:34 -0500

    add grid

--- src/components/SubSection.js
+++ src/components/SubSection.js
@@ -4,6 +4,17 @@ const SubSection = () => {
   return(
     <div>
       <h2>this is the sub section</h2>
+      <div class="grid-container">
+  <div class="grid-item">1</div>
+  <div class="grid-item">2</div>
+  <div class="grid-item">3</div>
+  <div class="grid-item">4</div>
+  <div class="grid-item">5</div>
+  <div class="grid-item">6</div>
+  <div class="grid-item">7</div>
+  <div class="grid-item">8</div>
+  <div class="grid-item">9</div>
+</div>
     </div>
   )
 }
