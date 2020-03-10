c751f98636e43868455bb906217f5eff1613a961
Josely castro <joselycas@gmail.com>
2020-03-10 13:08:31 -0400

    content update

--- src/components/SubSection.js
+++ src/components/SubSection.js
@@ -3,17 +3,17 @@ import React from "react";
 const SubSection = () => {
   return(
     <div>
-      <h2>this is the sub section</h2>
+      <h2>Areas of Focus</h2>
       <div class="grid-container">
-  <div class="grid-item">1</div>
-  <div class="grid-item">2</div>
-  <div class="grid-item">3</div>
-  <div class="grid-item">4</div>
-  <div class="grid-item">5</div>
-  <div class="grid-item">6</div>
-  <div class="grid-item">7</div>
-  <div class="grid-item">8</div>
-  <div class="grid-item">9</div>
+        <div class="grid-item">1</div>
+        <div class="grid-item">2</div>
+        <div class="grid-item">3</div>
+        <div class="grid-item">4</div>
+        <div class="grid-item">5</div>
+        <div class="grid-item">6</div>
+        <div class="grid-item">7</div>
+        <div class="grid-item">8</div>
+        <div class="grid-item">9</div>
 </div>
     </div>
   )
