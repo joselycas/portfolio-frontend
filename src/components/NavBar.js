19ae5046cef246fb509d34912b1b743782d430a3
Josely castro <joselycas@gmail.com>
2020-03-19 22:41:37 -0400

    add css

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -9,14 +9,14 @@ import "./index.css";
 
 const NavBar = () => {
  return(
-     <>
-   <ul>
-    <li><a href="default.asp" >Home</a></li>
-    <li><a href="news.asp">News</a></li>
-    <li><a href="contact.asp">Contact</a></li>
-    <li><a href="about.asp">About</a></li>
-  </ul>
-  </>
+   <div>
+      <ul>
+        <li><a href="default.asp" >Home</a></li>
+        <li><a href="news.asp">News</a></li>
+        <li><a href="contact.asp">Contact</a></li>
+        <li><a href="about.asp">About</a></li>
+      </ul>
+  </div>
  )
 }
 
