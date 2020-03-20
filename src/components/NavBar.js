cc1c78275eb97edf81fc1c4c6a855154d5f150fa
Josely castro <joselycas@gmail.com>
2020-03-19 22:14:13 -0400

    add logo

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,13 +2,15 @@ import React from "react";
 import Tabs from '@material-ui/core/Tabs';
 import AppBar from '@material-ui/core/AppBar';
 import Tab from '@material-ui/core/Tab';
+import './index.css'
 
 const NavBar = () => {
  return(
      <>
-    <AppBar position="static">
+    <AppBar position="static" className="navBar">
     <Tabs value="0" >
       <Tab label="Home" value="0"/>
+      <Tab label="Bio" value="0"/>
       <Tab label="Projects" value="0"/>
       <Tab label="Contact" value="0"/>
     </Tabs>
