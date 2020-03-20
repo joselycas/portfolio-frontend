5d47966a2412b737cb1cf9c9b3e659dc28c42d5f
Josely castro <joselycas@gmail.com>
2020-03-19 22:27:18 -0400

    update

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,22 +1,33 @@
 import React from "react";
-import Tabs from '@material-ui/core/Tabs';
-import AppBar from '@material-ui/core/AppBar';
-import Tab from '@material-ui/core/Tab';
-import './index.css'
+// import Tabs from '@material-ui/core/Tabs';
+// import AppBar from '@material-ui/core/AppBar';
+// import Tab from '@material-ui/core/Tab';
+import "./index.css";
+
+
+
 
 const NavBar = () => {
  return(
      <>
-    <AppBar position="static" className="navBar">
-    <Tabs value="0" >
-      <Tab label="Home" value="0"/>
-      <Tab label="Bio" value="0"/>
-      <Tab label="Projects" value="0"/>
-      <Tab label="Contact" value="0"/>
-    </Tabs>
-  </AppBar>
+   <ul>
+  <li><a href="default.asp" >Home</a></li>
+  <li><a href="news.asp">News</a></li>
+  <li><a href="contact.asp">Contact</a></li>
+  <li><a href="about.asp">About</a></li>
+</ul>
   </>
  )
 }
 
 export default NavBar;
+
+
+{/* <AppBar position="static" className="navBar">
+<Tabs value="0" >
+  <Tab className="navBar" label="Home" value="0"/>
+  <Tab className="navBar" label="Bio" value="0"/>
+  <Tab className="navBar" label="Projects" value="0"/>
+  <Tab className="navBar" label="Contact" value="0"/>
+</Tabs>
+</AppBar> */}
