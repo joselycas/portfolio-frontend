bc0e7a390429a7084c5a373306841c495db5508c
Josely castro <joselycas@gmail.com>
2020-03-20 11:13:03 -0400

    update css

--- src/components/SubSection.js
+++ src/components/SubSection.js
@@ -6,22 +6,43 @@ const SubSection = () => {
   return(
     <div>
       <h1><center>Areas of Focus</center></h1>
-      <div className="grid-container">
-        <div className="grid-item">Ruby/Rails</div>
-        <div className="grid-item">Javascript/React</div>
-        <div className="grid-item">Digital Marketing Stragegy</div>
-        <div className="grid-item">Marketing Campaign Design</div>
+      <div className="row">
+        <div className="column">
+          <p>Ruby/Rails</p><p>My projects are built with Ruby on Rails specifically for the backend of my applications. </p></div>
+        <div className="column">
+          <p>Javascript/React</p><p>The client-side of my projects are built using React framework with Redux for better management. </p></div>
+        <div className="column">
+          <p>CSS/Bootstrap</p> <p>The style of my projects are either manually designed or by using bootstrap. </p></div>
+        <div className="column"><p>Marketing Campaign and Web Design</p><p>Eight years of experience helping companies get their products out in the market through strategically planned digital channels. </p></div>
       </div>
-      <h2>Previous Clients and Employers</h2>
-      <div className="grid-container">
-        <div className="grid-item">American Standard</div>
-        <div className="grid-item">Nvent</div>
-        <div className="grid-item">Rakuten</div>
-        <div className="grid-item">FitLife</div>
-        <div className="grid-item">TNE Lab</div>
+      <h1><center>Previous Clients and Employers</center></h1>
+      <div className="row">
+        <div className="column">American Standard</div>
+        <div className="column">Nvent</div>
+        <div className="column">Rakuten</div>
+        <div className="column">FitLife</div>
+        <div className="column">TNE Lab</div>
       </div>
     </div>
   )
 }
 
 export default SubSection;
+
+// <div>
+// <h1><center>Areas of Focus</center></h1>
+// <div className="row">
+//   <div className="column">Ruby/Rails</div>
+//   <div className="column">Javascript/React</div>
+//   <div className="column">Digital Marketing Stragegy</div>
+//   <div className="column">Marketing Campaign Design</div>
+// </div>
+// <h2>Previous Clients and Employers</h2>
+// <div className="row">
+//   <div className="column">American Standard</div>
+//   <div className="column">Nvent</div>
+//   <div className="column">Rakuten</div>
+//   <div className="column">FitLife</div>
+//   <div className="column">TNE Lab</div>
+// </div>
+// </div>
