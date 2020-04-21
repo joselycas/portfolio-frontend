a1f965fcdb0e061d3f64e9b49faae29afad46a3e
Josely castro <joselycas@gmail.com>
2020-04-21 12:56:59 -0400

    add css to header content

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,13 +3,17 @@ import React from "react";
 
 const Header = () => {
   return(
-    <header className="row">
-      <div className="column">
-          {/* <img src="assets/header.png" alt="Header" className="partnerLogo" />       */}
-      </div>
-      <div className="column">
-          <h2 className="title">FULL-STACK WEB DEVELOPER AND DIGITAL STRATEGIST.</h2>
-          <button>PROJECTS</button>
+    <header>
+      <div className="row">
+        <div className="column">
+            <img src="assets/header.png" alt="Header" />      
+        </div>
+        <div className="column">
+            <h2 className="title">FULL-STACK WEB DEVELOPER AND DIGITAL STRATEGIST.</h2>
+            <div className="buttonHolder">
+              <button>PROJECTS</button>
+            </div>
+        </div>
       </div>
     </header>
   )
