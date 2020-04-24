71be582fb98e3d2c577a922b7575fb7b76e82557
Josely castro <joselycas@gmail.com>
2020-04-24 11:53:40 -0400

    update order of nav bar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -8,10 +8,10 @@ const NavBar = () => {
    <div>
      <nav>
       <ul>
-        <li><a href="default.asp" >HOME</a></li>
+        <li><a href="https://joscastro.com/contact" >CONTACT</a></li>
         <li><a href="https://joscastro.com/about">BIO</a></li>
         <li><a href="https://joscastro.com/projects">PROJECTS</a></li>
-        <li><a href="https://joscastro.com/contact">CONTACT</a></li>
+        <li><a href="https://joscastro.com/contact">HOME</a></li>
       </ul>
     </nav>
   </div>
